3679f098347bd3d17436780c139e42f464f282fd
arsheenkour <arsheenkour47@gmail.com>
2024-06-08 10:51:21 +0530

    first commit

---
+++ conditionalstat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"enter the value in the variables"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cout<<(a>b||c<d);
+}
